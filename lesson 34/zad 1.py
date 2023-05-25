b7c27d9640d0fe05ce96ae6308a33088aa24787a
movavi_school <Tanahu2010@gmail.com>
2023-05-25 11:45:06 +0700

    25 мая

---
+++ lesson 34/zad 1.py
@@ -0,0 +1,46 @@
+# Задача 1
+# slovar = {"key1": "value1",
+#           "key2": "value2",
+#           "key3": "value3",
+#           "key4": "value4",
+#           "key5": "value5"}
+# keys = list(slovar.keys())
+#
+# values = list(slovar.values())
+# print(keys)
+# print(values)
+
+# slovar = {"key1": "value1",
+#           "key2": "value2",
+#           "key3": "value3",
+#           "key4": "value4",
+#           "key5": "value5"}
+#
+#
+# keys = []
+# val = []
+# for (key, value) in slovar.items():
+#     keys.append(key)
+#     val.append(value)
+# print(keys)
+# print(val)
+
+
+# Задача 2
+#
+# x = ["Никита", "Екатерина", "Арсалан", "Наталья", "Григорий", "Евгений", "Анастасия", "Андрей", "Евгения", "Герман", "Тимур", "Ярослава", "Есения", "Даниил", "Данил"]
+#
+# word = input().capitalize()
+# count = x.count(word)
+# print(count)
+
+# x = ["Никита", "Екатерина", "Арсалан", "Наталья", "Григорий", "Евгений", "Анастасия", "Андрей", "Евгения", "Герман", "Тимур", "Ярослава", "Есения", "Даниил", "Данил"]
+# word = input().capitalize()
+# kol = 0
+# for i in x:
+#     if word == i:
+#         kol +=1
+#
+#
+# print(kol)
+type
